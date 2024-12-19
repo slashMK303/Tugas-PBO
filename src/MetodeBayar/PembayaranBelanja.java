c071f078a8b2cf3ab5231558434750558cf00282
andikariskys <andikarisky313@gmail.com>
2024-12-19 11:54:14 +0700

    Pembayaran finished

--- src/MetodeBayar/PembayaranBelanja.java
+++ src/MetodeBayar/PembayaranBelanja.java
@@ -5,81 +5,86 @@ package MetodeBayar;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author hp
  */
-public class PembayaranBelanja implements MetodeBayarPakaiAplikasi{
-    double Kembalian;
-    double Payment;
+public class PembayaranBelanja implements MetodeBayarPakaiAplikasi {
+
+    double Kembalian, Payment;
+    int Id_user, Pin, Rekening;
+
     @Override
     public double BayarGoPay(int id_user, double saldo, double BelanjaanTotal) {
-        if(saldo >= BelanjaanTotal){
-            Payment = saldo - BelanjaanTotal;}
+        Id_user = id_user;
+        Payment = saldo - BelanjaanTotal;
+
         return Payment;
     }
 
     @Override
     public double BayarShopee(int id_user, double saldo, double BelanjaanTotal) {
-        if(saldo >= BelanjaanTotal){
-            Payment = saldo - BelanjaanTotal;}
+        Id_user = id_user;
+        Payment = saldo - BelanjaanTotal;
+
         return Payment;
     }
 
     @Override
     public double BayarCash(double cash, double belanja) {
-        Kembalian = cash - belanja; 
+        Kembalian = cash - belanja;
+        
         return Kembalian;
     }
 
     @Override
     public double BayarQris(double saldo, String kode, double totalBelanja) {
-    if(saldo >= totalBelanja){
-            Payment = saldo - totalBelanja;}
-    if(kode == kode){
-        System.out.println("Kode Benar");}
-        return Payment;}
+        int potongan;
+        switch (kode) {
+            case "Diskon10":
+                potongan = 10;
+                break;
+            case "Diskon25":
+                potongan = 25;
+                break;
+            case "Diskon50":
+                potongan = 50;
+                break;
+            default:
+                potongan = 0;
+                break;
+        }
+        Payment = saldo - totalBelanja;
+        double diskon = (potongan * Payment) / 100;
+
+        Payment = Payment - diskon;
+        return Payment;
+    }
 
     @Override
     public double BayareWallet(double saldo, int pin, int rekening, double totalBelanja) {
-    if(saldo >= totalBelanja){
-            Payment = saldo - totalBelanja;}
-    if(pin == pin){
-            System.out.println("Pin Benar");}
-    if(rekening == rekening){
-            System.out.println("-");}
-        else{
-        System.out.println("Rekening Benar");
-    }  
+        Pin = pin;
+        Rekening = rekening;
+        Payment = saldo - totalBelanja;
+
         return Payment;
     }
 
     @Override
     public double BayarKreditCard(double saldo, int pin, int rekening, double totalBelanja) {
-    if(saldo >= totalBelanja){
-            Payment = saldo - totalBelanja;}
-    if(pin == pin){
-            System.out.println("Pin Benar");}
-    if(rekening == rekening){
-            System.out.println("-");}
-        else{
-        System.out.println("Rekening Benar");
-    }  
+        Pin = pin;
+        Rekening = rekening;
+        Payment = saldo - totalBelanja;
+
         return Payment;
     }
 
     @Override
     public double BayarDebitCard(double saldo, int pin, int rekening, double totalBelanja) {
-    if(saldo >= totalBelanja){
-            Payment = saldo - totalBelanja;}
-    if(pin == pin){
-            System.out.println("Pin Benar");}
-    if(rekening == rekening){
-            System.out.println("-");}
-        else{
-        System.out.println("Rekening Benar");
-    }  
+        Pin = pin;
+        Rekening = rekening;
+        Payment = saldo - totalBelanja;
+
         return Payment;
     }
 }
